263018ed6410bc6d79f6e10cd228f3ff144b68bd
Greenduh <greendsmain@gmail.com>
2025-02-08 17:51:27 +0200

    add miles to km converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,18 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("My unit converter app.");
+        System.out.print("Enter miles to be converted into km: ");
+        Scanner scanner = new Scanner(System.in);
+        double miles = scanner.nextDouble();
+        System.out.println(miles + " miles = " + convertMilesToKm(miles) + " km");
+    }
+
+    static double convertMilesToKm(double miles) {
+        return miles * 1.60934;
     }
 }
